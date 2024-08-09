e4d13288c577748c8e7aee27c3fefd2b3214061b
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-09 14:29:29 +0800

    feat: init email.utils.js

---
+++ backend/email.utils.js
@@ -0,0 +1,7 @@
+function sendEmail(oldPrice, newPrice, address) {
+    // send email to single address
+}
+
+function sendEmailAll(oldPrice, newPrice) {
+    // send email to all emails in database
+}
