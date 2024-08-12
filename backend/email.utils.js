e3a13134ef6884667f845b4972b787ff467f523a
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-12 09:09:18 +0800

    feat: create save email function

--- backend/email.utils.js
+++ backend/email.utils.js
@@ -1,3 +1,10 @@
+import sql from './db.js'
+
+async function saveEmail(address) {
+    // save email address
+    await sql`INSERT INTO emails VALUES (${address});`
+}
+
 function sendEmail(oldPrice, newPrice, address) {
     // send email to single address
 }
