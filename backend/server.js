90a372cfd62650047ac37fb02f139fc3f4c1c92d
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-12 15:40:15 +0800

    feat: add cors to backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from 'cors';
 
 import emailUtils from "./email.utils.js";
 
 const PORT = 6900;
 const app = express();
+app.use(cors());
 
 app.post('/email/save', async (req, res) => {
     const address = req.query.address;
