b3cd2a52bbb1011477a74a1bff33f1db18775141
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-12 15:16:18 +0800

    feat: create save email api

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
 
+import emailUtils from "./email.utils.js";
+
 const PORT = 6900;
 const app = express();
+
+app.post('/email/save', async (req, res) => {
+    const address = req.query.address;
+    await emailUtils.saveEmail(address)
+})
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
