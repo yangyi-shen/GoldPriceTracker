0de384b9623dd30847fd7ef51b430aab5920dd19
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-13 09:30:25 +0800

    feat: add error handling js

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,31 @@
 const emailForm = document.getElementById('email-form');
 const emailInput = document.getElementById('email-input');
+const errorMessage = document.getElementById('error-message')
+
+errorMessage.style.display = 'none';
 
 const URL = 'http://localhost:6900'
 
 emailForm.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const emailValue = emailInput.value;
-    await fetch(`${URL}/email/save?address=${emailValue}`, {
+    const emailValue = emailInput.value.toLowerCase();
+    const response = await fetch(`${URL}/email/save?address=${emailValue}`, {
         method: 'POST'
     })
+        .then(response => response.json())
+
+    if (response) {
+        errorMessage.style.display = 'none';
+
+        fetch('/frontend/registered.html')
+            .then(response => response.text())
+            .then(html => {
+                document.write(html);
+            })
+    } else {
+        errorMessage.style.display = 'block';
+    }
 
     emailInput.value = '';
 });
