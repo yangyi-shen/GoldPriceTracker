7643d94f7996bbc790d748e6fe7c633d9c594eff
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-15 13:12:56 +0800

    feat: add header to goldprice api request

--- backend/getgoldprice.js
+++ backend/getgoldprice.js
@@ -1,7 +1,10 @@
 const API_URL = 'https://data-asg.goldprice.org/dbXRates/USD'
+const HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0"
+}
 
 export default async function getGoldPrice() {
-    const response = await fetch(API_URL)
+    const response = await fetch(API_URL, HEADERS)
         .then(response => response.json())
         .then(response => response.items[0].xauPrice)
 
