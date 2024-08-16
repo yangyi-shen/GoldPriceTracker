ec6662b004147be083be57c0bec0cb1ac7b72d12
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-16 15:10:35 +0800

    feat: add loading icon

--- frontend/script.js
+++ frontend/script.js
@@ -2,20 +2,25 @@ const emailForm = document.getElementById('email-form');
 const emailInput = document.getElementById('email-input');
 const errorMessage = document.getElementById('error-message')
 const spamMessage = document.getElementById('spam-message')
+const loadingIcon = document.getElementById('loading-icon')
 
 errorMessage.style.display = 'none';
 spamMessage.style.display = 'block';
+loadingIcon.style.display = 'none'
 
 const URL = 'http://localhost:6900'
 
 emailForm.addEventListener('submit', async (event) => {
     event.preventDefault();
+    loadingIcon.style.display = 'inline-block'
 
     const emailValue = emailInput.value.toLowerCase();
     const response = await fetch(`${URL}/email/save?address=${emailValue}`, {
         method: 'POST'
     })
         .then(response => response.json())
+        
+    loadingIcon.style.display = 'none'
 
     if (response) {
         errorMessage.style.display = 'none';
