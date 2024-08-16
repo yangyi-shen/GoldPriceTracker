494279ce5253d3e3b3d5aca69628f6fa0770be86
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-08-16 10:04:29 +0800

    feat(frontend): adjust alerts

--- frontend/unsubscribed.js
+++ frontend/unsubscribed.js
@@ -1,19 +1,17 @@
-const unsubscribe = confirm('Are you sure you want to unsubscribe? 🥺')
+const unsubscribe = confirm('Are you sure you want to unsubscribe? 🥺');
 
-const URL = 'http://localhost:6900'
+const URL = 'http://localhost:6900';
 
 if (unsubscribe) {
-    const emailValue = prompt('Please enter your email.')
+    const emailValue = prompt('Please enter your email.');
 
     if (emailValue) {
         // delete the email
         await fetch(`${URL}/email/delete?address=${emailValue}`, {
             method: 'DELETE'
-        })
-
-        alert('Unsubscribe successful.')
+        });
     } else {
         // close tab
-        window.close()
+        window.close();
     }
 }
